9430c33b9794446209adb344c5ee9b1e3a2386f8
danielle frankel <daniellefrankel@danielle-frankels-MacBook-2.local>
2018-05-03 18:44:24 -0400

    schema update

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,9 @@ const MySQL = require('mysql');
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-
     user: "root",
     password: "root",
     database: "burgersDB",
-
     socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
 });
 
